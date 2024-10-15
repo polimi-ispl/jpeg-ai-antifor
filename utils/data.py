59c3dd0a9322a66d72009e6f9fedcc7b1be91bf2
Edoardo Daniele Cannas <edoardodaniele.cannas@polimi.it>
2024-10-15 17:32:41 +0200

    added complete dataloader

--- utils/data.py
+++ utils/data.py
@@ -13,10 +13,23 @@ from PIL import Image
 import sys
 from torchvision import transforms as T
 import pandas as pd
+from typing import List
 
 # --- Helpers functions and classes --- #
+
+def get_transform_list(detector: str):
+    if detector == 'Grag2021_progan':
+        return T.Compose([T.ToTensor(),T.Normalize(mean=[0.485, 0.456, 0.406],
+                                                    std=[0.229, 0.224, 0.225])])
+    elif detector == 'Grag2021_latent':
+        return T.Compose([T.ToTensor(),T.Normalize(mean=[0.485, 0.456, 0.406],
+                                                    std=[0.229, 0.224, 0.225])])
+    else:
+        return T.Compose([T.ToTensor()])
+
+
 class JPEGAIDataset(torch.utils.data.Dataset):
-    def __init__(self, root_dir, transform: T.Transform=None, data_df: pd.DataFrame=None):
+    def __init__(self, root_dir: str, data_df: pd.DataFrame, transform: torch.nn.Module=None):
         self.root_dir = root_dir
         self.transform = transform
         self.data_df = data_df
@@ -29,4 +42,4 @@ class JPEGAIDataset(torch.utils.data.Dataset):
         image = Image.open(img_path)
         if self.transform:
             image = self.transform(image)
-        return image, torch.Tensor([label], dtype=torch.float32)
+        return image, torch.Tensor([label])
