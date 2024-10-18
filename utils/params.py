31a782049a25e729fe42428c42d620c9cd12e616
Edoardo Daniele Cannas <edoardodaniele.cannas@polimi.it>
2024-10-18 11:46:37 +0200

    added test data

--- utils/params.py
+++ utils/params.py
@@ -18,7 +18,10 @@ MODELS_LIST = {'Grag2021_progan': 'Grag2021_progan',
 TEST_DATA = {'Grag2021_progan': ['imagenet', 'coco'],
              'Grag2021_latent': ['imagenet', 'coco'],
              'Ohja2023': ['imagenet', 'coco', 'lsun', 'laion', 'raise', 'celeba'],
-             'Ohja2023ResNet50': ['imagenet', 'coco', 'lsun', 'laion', 'raise', 'celeba']}
+             'Ohja2023ResNet50': ['imagenet', 'coco', 'lsun', 'laion', 'raise', 'celeba'],
+             'CLIP2024': ['lsun', 'ffhq', 'imagenet', 'coco', 'laion', 'raise'],
+             'CLIP2024Plus': ['lsun', 'ffhq', 'imagenet', 'coco', 'laion', 'raise'],
+             'Corvi2023': ['lsun', 'ffhq', 'imagenet', 'coco', 'laion', 'raise']}
 
 DETECTORS = ['Grag2021_progan', 'Grag2021_latent', 'Ohja2023', 'Ohja2023ResNet50',
              'CLIP2024', 'CLIP2024Plus', 'Corvi2023']
