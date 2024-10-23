d833df8da94163ac362b7c1fcbe1bac2b2ba240d
Edoardo Daniele Cannas <edoardodaniele.cannas@polimi.it>
2024-10-23 10:59:21 +0200

    added centercrop for Corvi2023

--- utils/data.py
+++ utils/data.py
@@ -37,8 +37,9 @@ def get_transform_list(detector: str):
                           T.Normalize(mean=[0.48145466, 0.4578275, 0.40821073],
                                       std=[0.26862954, 0.26130258, 0.27577711])])
     elif detector == 'Corvi2023':
-        return T.Compose([T.ToTensor(), T.Normalize(mean=[0.485, 0.456, 0.406],
-                                                    std=[0.229, 0.224, 0.225])])
+        return T.Compose([T.CenterCrop(256), T.ToTensor(),
+                          T.Normalize(mean=[0.485, 0.456, 0.406],
+                                      std=[0.229, 0.224, 0.225])])
     elif detector in ['Wang2020JPEG01', 'Wang2020JPEG05']:
         return T.Compose([T.CenterCrop(224), T.ToTensor(),
                           T.Normalize(mean=[0.485, 0.456, 0.406],
