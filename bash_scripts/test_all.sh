0b7ef1cc53d82f8c3c0585e7d46b6a571fde9c2c
Edoardo Daniele Cannas <edoardodaniele.cannas@polimi.it>
2025-01-05 12:14:21 +0100

    bash script for testing all the models

---
+++ bash_scripts/test_all.sh
@@ -0,0 +1,112 @@
+#!/usr/bin/env bash
+# Simple script to test all the detectors considered in the paper
+
+# USER PARAMETERS (put your device configuration params here)
+DEVICE=0  # PUT THERE YOUR GPU ID
+DATA_DIR='/nas/public/exchange/JPEG-AI/data'  # PUT THERE THE PATH TO THE TEST SET
+RESULTS_DIR='./results'  # PUT THERE THE PATH TO THE RESULTS FOLDER
+
+echo ""
+echo "-------------------------------------------------"
+echo "| Testing Wang2020-A for all tasks |"
+echo "-------------------------------------------------"
+python ../test_detector.py --input_dir=${DATA_DIR} --output_dir=${RESULTS_DIR} --gpu=${DEVICE} --detector Wang2020-A --weights_path ../utils/third_party/Wang2020CNNDetection/weights --batch_size 128 --test_all
+echo ""
+echo "-------------------------------------------------"
+echo "| Computing metrics for Wang2020-A |"
+echo "-------------------------------------------------"
+python ../utils/compute_metrics.py --results_dir=${RESULTS_DIR} --detector Wang2020-A
+echo ""
+echo "-------------------------------------------------"
+echo "| Wang2020-A done! |"
+echo "-------------------------------------------------"
+
+echo ""
+echo "-------------------------------------------------"
+echo "| Testing Wang2020-B for all tasks |"
+echo "-------------------------------------------------"
+python ../test_detector.py --input_dir=${DATA_DIR} --output_dir=${RESULTS_DIR} --gpu=${DEVICE} --detector Wang2020-B --weights_path ../utils/third_party/Wang2020CNNDetection/weights --batch_size 128 --test_all
+echo ""
+echo "-------------------------------------------------"
+echo "| Computing metrics for Wang2020-B |"
+echo "-------------------------------------------------"
+python ../utils/compute_metrics.py --results_dir=${RESULTS_DIR} --detector Wang2020-B
+echo ""
+echo "-------------------------------------------------"
+echo "| Wang2020-B done! |"
+echo "-------------------------------------------------"
+
+echo ""
+echo "-------------------------------------------------"
+echo "| Testing Gragnaniello2021 for all tasks |"
+echo "-------------------------------------------------"
+python ../test_detector.py --input_dir=${DATA_DIR} --output_dir=${RESULTS_DIR} --gpu=${DEVICE} --detector Grag2021_progan --weights_path ../utils/third_party/DMImageDetection_test_code/weights --batch_size 128 --test_all
+echo ""
+echo "-------------------------------------------------"
+echo "| Computing metrics for Gragnaniello2021 |"
+echo "-------------------------------------------------"
+python ../utils/compute_metrics.py --results_dir=${RESULTS_DIR} --detector Grag2021_progan
+echo ""
+echo "-------------------------------------------------"
+echo "| Gragnaniello2021 done! |"
+echo "-------------------------------------------------"
+
+echo ""
+echo "-------------------------------------------------"
+echo "| Testing Corvi2023 for all tasks |"
+echo "-------------------------------------------------"
+python ../test_detector.py --input_dir=${DATA_DIR} --output_dir=${RESULTS_DIR} --gpu=${DEVICE} --detector Corvi2023 --weights_path ../utils/third_party/ClipBased_SyntheticImageDetection_main/weights --batch_size 128 --test_all
+echo ""
+echo "-------------------------------------------------"
+echo "| Computing metrics for Corvi2023 |"
+echo "-------------------------------------------------"
+python ../utils/compute_metrics.py --results_dir=${RESULTS_DIR} --detector Corvi2023
+echo ""
+echo "-------------------------------------------------"
+echo "| Corvi2023 done! |"
+echo "-------------------------------------------------"
+
+echo ""
+echo "-------------------------------------------------"
+echo "| Testing Ojha2023 for all tasks |"
+echo "-------------------------------------------------"
+python ../test_detector.py --input_dir=${DATA_DIR} --output_dir=${RESULTS_DIR} --gpu=${DEVICE} --detector Ojha2023 --weights_path ../utils/third_party/UniversalFakeDetect_test_code/pretrained_weights --batch_size 128 --test_all
+echo ""
+echo "-------------------------------------------------"
+echo "| Computing metrics for Ojha2023 |"
+echo "-------------------------------------------------"
+python ../utils/compute_metrics.py --results_dir=${RESULTS_DIR} --detector Ojha2023
+echo ""
+echo "-------------------------------------------------"
+echo "| Ojha2023 done! |"
+echo "-------------------------------------------------"
+
+echo ""
+echo "-------------------------------------------------"
+echo "| Testing Cozzolino2024-A for all tasks |"
+echo "-------------------------------------------------"
+python ../test_detector.py --input_dir=${DATA_DIR} --output_dir=${RESULTS_DIR} --gpu=${DEVICE} --detector Cozzolino2024-A --weights_path ../utils/third_party/ClipBased_SyntheticImageDetection_main/weights --batch_size 128 --test_all
+echo ""
+echo "-------------------------------------------------"
+echo "| Computing metrics for Cozzolino2024-A |"
+echo "-------------------------------------------------"
+python ../utils/compute_metrics.py --results_dir=${RESULTS_DIR} --detector Cozzolino2024-A
+echo ""
+echo "-------------------------------------------------"
+echo "| Cozzolino2024-A done! |"
+echo "-------------------------------------------------"
+
+echo ""
+echo "-------------------------------------------------"
+echo "| Testing Cozzolino2024-B for all tasks |"
+echo "-------------------------------------------------"
+python ../test_detector.py --input_dir=${DATA_DIR} --output_dir=${RESULTS_DIR} --gpu=${DEVICE} --detector Cozzolino2024-B --weights_path ../utils/third_party/ClipBased_SyntheticImageDetection_main/weights --batch_size 128 --test_all
+echo ""
+echo "-------------------------------------------------"
+echo "| Computing metrics for Cozzolino2024-B |"
+echo "-------------------------------------------------"
+python ../utils/compute_metrics.py --results_dir=${RESULTS_DIR} --detector Cozzolino2024-B
+echo ""
+echo "-------------------------------------------------"
+echo "| Cozzolino2024-B done! |"
+echo "-------------------------------------------------"
